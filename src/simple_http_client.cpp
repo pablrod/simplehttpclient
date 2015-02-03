d5a2b98fe5df075f690c92d86b2f9ec6bec5156a
Pablo Rodríguez González <pablo.rodriguez.gonzalez@gmail.com>
2015-02-03 21:06:26 +0100

    Synchronous HTTP Request and dump to stdout

--- src/simple_http_client.cpp
+++ src/simple_http_client.cpp
@@ -11,28 +11,56 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <boost/asio.hpp>
 
 using namespace std;
 
-void ParseCommandLine(int argc, const char *argv[], string &host, int &port);
+void ParseCommandLine(int argc, const char *argv[], string &host, int &port, string &path);
 
 int main(int argc, const char *argv[]) {
 	int port;
 	string host;
-	ParseCommandLine(argc, argv, host, port);
+	string path;
+	ParseCommandLine(argc, argv, host, port, path);
+
+	try {
+		using namespace boost::asio;
+		using namespace boost::asio::ip;
+
+		io_service communication_service;
+		auto server = tcp::resolver(communication_service).resolve(
+				boost::asio::ip::tcp::resolver::query(host, to_string(port)));
+
+		tcp::socket connection(communication_service);
+		connect(connection, server);
+		boost::asio::streambuf request;
+		std::ostream request_stream(&request);
+		request_stream << "GET " << path << " HTTP/1.0\r\n";
+		request_stream << "Host: " << host << "\r\n";
+		request_stream << "Accept: */*\r\n";
+		request_stream << "Connection: close\r\n\r\n";
+		write(connection, request);
+		boost::asio::streambuf response;
+		read_until(connection, response, "\r\n");
+
+		cout << &response << endl;
+	} catch (exception &e) {
+		cerr << "An exception has ocurred: " << e.what() << endl;
+	}
 
 	return 0;
 }
 
-void ParseCommandLine(int argc, const char *argv[], string &host, int &port) {
+void ParseCommandLine(int argc, const char *argv[], string &host, int &port, string &path) {
 
 	using namespace boost::program_options;
 	using boost::program_options::error;
 
 	options_description command_line_description("Arguments");
-	command_line_description.add_options()
-	("port", value<int>(&port)->default_value(80), "Port number")
-	("host", value<string>(&host)->required(), "Host name: www.example.com");
+	command_line_description.add_options()("port",
+			value<int>(&port)->default_value(80), "Port number")("host",
+			value<string>(&host)->required(), "Host name: www.example.com")
+			("path", value<string>(&path)->default_value("/", "Server path"));
 
 	try {
 		variables_map arguments;
